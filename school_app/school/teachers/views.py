b3d23def82a9dd6a01c09ebb397f9303bb229ab4
Daniel Pérez <daniel_p0818@hotmail.com>
2023-05-09 20:34:48 -0500

    next class data dinamically generated

--- school_app/school/teachers/views.py
+++ school_app/school/teachers/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
-from django.db.models.functions import ExtractWeek
+from django.db.models.functions import ExtractWeek, ExtractDay, ExtractWeekDay
 
 from datetime import datetime, date, timedelta, tzinfo, time
 from zoneinfo import ZoneInfo
@@ -27,12 +27,10 @@ def home(request):
     print(len(schedule_classes))
     #for x in schedule_classes[0]: 
     x = schedule_classes[0].schedulesubjects_set.all()  
-    print(len(x)) 
-    print(x[0].weekday)
+    
     week_classes = []
     for subject in schedule_classes:
         for x in subject.schedulesubjects_set.all():
-            print(x.weekday)
             daytime = datetime.fromisocalendar(today.year,week,x.weekday_iso) 
             #print(day[1])
             time_ = time.fromisoformat(x.start)
@@ -48,8 +46,22 @@ def home(request):
         upcoming_class = week_classes[0] + timedelta(weeks=1)
     else:
         upcoming_class = next_classes[0]
+    print(upcoming_class)
+    
+    upcoming_class_time = str(upcoming_class.time())
+    
+    upcoming_class_str = upcoming_class.strftime("%b %d %Y at %H:%M")
 
-    upcoming_class = upcoming_class.strftime("%b %d %Y at %H:%M")
+    upcoming_class_day = datetime.isocalendar(upcoming_class)[2]
+
+    print(upcoming_class_day,upcoming_class_time)
+    upcoming_class_data = ScheduleSubjects.objects.filter(
+        weekday_iso__gte = upcoming_class_day, 
+        start = upcoming_class_time,grade_subject__teacher__id = request.user.id).first()
+
+    bach = ScheduleSubjects.objects.filter(grade_subject__teacher__id = request.user.id).all()
+    print(len(bach),'NTO')
+    print(upcoming_class_data.id)
     announcements = Announcements.objects.annotate(week=ExtractWeek('date')).filter(
         week=week, date__year = today.year).all()
     
@@ -62,17 +74,23 @@ def home(request):
         laboratories = len( [x for x in events if x.event_type == 'laboratory'])
         assessments = len([x for x in events if x.event_type == 'exam'])
 
-    reports = db.session.execute("SELECT COUNT(report_id) FROM reports WHERE "
-        "WEEK(created_at,3) = WEEK(CURRENT_DATE,3)")
+    # reports = db.session.execute("SELECT COUNT(report_id) FROM reports WHERE "
+    #     "WEEK(created_at,3) = WEEK(CURRENT_DATE,3)")
     reports = Reports.objects.annotate(week=ExtractWeek('created_at')).filter(
-        week=week, date__year = today.year).all() 
+        week=week, created_at__year = today.year).all() 
 
     lessons = len(schedule_classes)
     events = len(events)
     issued_announcements = len(announcements)
     # z = [x.name for x in children]
     jack = [ 'all work and no play makes jack a dull boy' for _ in range(1,100)]
-    context = {'data':jack,'next':upcoming_class}
+
+    context = {'data':jack,'next':{'date':upcoming_class_str,'data':upcoming_class_data},
+               'lessons':lessons,'events':events,
+               'laboratories':laboratories,'assessments':assessments,
+               'issued_announcements':issued_announcements,'reports':reports,
+               'monday':monday,'friday':friday}
+
     return render (request,'teachers/home.html',context)
 
 @login_required(login_url='/auth/login/')
